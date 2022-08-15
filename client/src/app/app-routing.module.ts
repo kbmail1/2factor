c772bb219058e80de37bbce7f84b31c6b2a1a90a
kundan Bapat <kundan.bapat@gmail.com>
2022-08-15 17:57:43 -0400

    canActivate added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HelloComponent } from './components/hello/hello.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'api/hello', component: HelloComponent },
-  { path: 'api/hello/:id', component: HelloComponent },
+  { path: 'api/hello', component: HelloComponent, canActivate: [AuthGuard] },
+  { path: 'api/hello/:id', component: HelloComponent, canActivate: [AuthGuard]},
   { path: '404', component: PagenotfoundComponent},
   { path: '**', redirectTo: '/404'}
 ]
---
+++ client/src/app/services/auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  loginState: boolean = false
+  loginSubscription: Subscription
+
+  constructor(
+    public loginService: LoginService,
+    private router: Router,
+  ) {
+    this.loginSubscription = this.loginService.loginStatus$.subscribe((status) => {
+      this.loginState = status
+      console.log('auth.guard in constructor: status:', status)
+    })
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    console.log('canActivate: ', this.loginState)
+    if (this.loginState) {
+      return true
+    } else {
+      this.router.navigate(['/home'])
+
+    }
+    return this.loginState
+  }
+
+}
