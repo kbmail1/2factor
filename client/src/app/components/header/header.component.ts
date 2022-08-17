11ab763f4483ece668535815a2bb499b0661e7db
kundan Bapat <kundan.bapat@gmail.com>
2022-08-17 10:33:13 -0400

    css cleanup

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoginService } from '../../services/login.service'
 import { Subscription } from 'rxjs';
+import { RestService } from 'src/app/services/rest.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +13,12 @@ export class HeaderComponent implements OnInit {
 
   loginState: boolean = false
   loginSubscription: Subscription
+  helloResponse: String|null = null
 
   constructor(
-    public loginService: LoginService
+    public loginService: LoginService,
+    public restService: RestService,
+    public router: Router,
   ) {
     this.loginSubscription = this.loginService.loginStatus$.subscribe((status) => {
       this.loginState = status
@@ -27,4 +32,19 @@ export class HeaderComponent implements OnInit {
   ngOnDestroy(): void {
     this.loginSubscription.unsubscribe()
   }
+
+  handleHello(): void {
+    this.restService.hello().subscribe((data) => {
+      this.helloResponse = JSON.stringify(data)
+      console.log(`HeaderComponent: hello: ${this.helloResponse}`)
+
+      // this.router.navigate(['/hello/665'], { queryParams: { 'hello-api-response': this.helloResponse } });
+      // this.router.navigate(['/hello'], { queryParams: { 'hello-api-response': this.helloResponse } });
+    })
+  }
+
+  handleLogin(): void {
+
+  }
+
 }
